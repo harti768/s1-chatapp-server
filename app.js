33fafb0939bee33a49c365b8bceffcb61306ef9b
Felix Rudat <frudat@doegel.de>
2016-04-05 17:25:38 +0200

    added basic channel api

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var jsonfile = require('jsonfile');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/channels', function(req, res){
+    var file = 'db/channels.json';
+    res.send(jsonfile.readFileSync(file));
 });
 
 io.on('connection', function(socket){
