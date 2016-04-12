a63d7e915edb39b4b9db8e71ae4855ad0ba1343b
Felix Rudat <frudat@doegel.de>
2016-04-12 18:06:58 +0200

    very experimental login to test app against

--- app.js
+++ app.js
@@ -15,6 +15,37 @@ app.get('/channels', function(req, res){
     res.send(jsonfile.readFileSync(file));
 });
 
+app.get('/login', function(req, res){
+    try {
+        // TODO this is a highly experimental login it should never reach production in this state
+        var user = require('./lib/login')
+            .login(req.query.username, req.query.password);
+
+        res.send({
+            token: user.token,
+            name: user.name,
+            thumbnail: user.thumbnail
+        });
+    } catch(e){
+        res.status(401).send({ error: 'invalid credentials' });
+    }
+});
+
+app.get('/auth', function(req, res){
+    try {
+        // TODO this is a highly experimental login it should never reach production in this state
+        var user = require('./lib/login')
+            .authorize(req.query.token);
+
+        res.send({
+            name: user.name,
+            thumbnail: user.thumbnail
+        });
+    } catch(e){
+        res.status(401).send({ error: 'invalid token' });
+    }
+});
+
 io.on('connection', function(socket){
     // connect
 
